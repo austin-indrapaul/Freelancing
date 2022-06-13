868baf47fbd4a1e12b4a2a6d4f4ca7e500cc1b97
Austin <austin.indrapaul@gmail.com>
2022-06-13 22:24:08 +0530

    New Changes made on header.

--- Bitek Web/js-files/homepage-components/HeaderComponent.js
+++ Bitek Web/js-files/homepage-components/HeaderComponent.js
@@ -1,17 +1,13 @@
-let header_heading = "Header Component - Heading"
-let header_description = "Header Component - description of the heading"
-let header_home = "homepage.html"
+let header_heading = "Bitek Web Technologies"
+let header_description = "We make your life easier"
 
 
 
 function HeaderComponent(){
     return(
-        <div id="div1">
-            <ul id= "ul1">
-            <li id = "il1"><h1>{header_heading}</h1></li>
-            <li id = "il1"><p>{header_description}</p></li>
-            <li id = "il1"><a href={header_home}>Home</a></li>
-            </ul>
+        <div id="header-content">
+            <h1>{header_heading}</h1>
+            <p>{header_description}</p>
         </div>   
     );
 }
