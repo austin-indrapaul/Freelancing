d061b47339b09f10be45e7e89ee818898661c701
DESKTOP-QDKPS8D\Binoj <binojjoeljoshua@gmail.com>
2022-06-13 18:07:25 +0530

    Header changes

--- Bitek Web/js-files/homepage-components/HeaderComponent.js
+++ Bitek Web/js-files/homepage-components/HeaderComponent.js
@@ -5,7 +5,7 @@ let header_description = "Header Component - description of the heading"
 
 function HeaderComponent(){
     return(
-        <div>
+        <div id="div1">
             <h1>{header_heading}</h1>
             <p>{header_description}</p>
         </div>   
