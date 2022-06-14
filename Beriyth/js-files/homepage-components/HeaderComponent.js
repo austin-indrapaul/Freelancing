7130da3347ef4bb68d70b0bbd1027693a43399cb
DESKTOP-QDKPS8D\Binoj <binojjoeljoshua@gmail.com>
2022-06-14 12:26:06 +0530

    Footer Changes

--- Beriyth/js-files/homepage-components/HeaderComponent.js
+++ Beriyth/js-files/homepage-components/HeaderComponent.js
@@ -1,10 +1,22 @@
 let header_heading = "Beriyth Info Tech"
 let header_description = "Simple creating opportunities and enabling prosperity"
 
+function navComponent(){
+    return (
+        <div id = "nav">
+            <ul>
+                <li><a class="active" href="#home">Home</a></li>
+                <li><a href="#news">News</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li><a href="#about">About</a></li>
+            </ul>
+        </div>
+    );
+}
 
 
 function HeaderComponent(){
-    return(
+    return (   
         <div id="header-content">
             <h1>{header_heading}</h1>
             <p>{header_description}</p>
