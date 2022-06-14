57cebc3c5641326431e35da4e48b44b5c55e737f
Austin <austin.indrapaul@gmail.com>
2022-06-14 15:32:29 +0530

    Footer component modified

---
+++ Beriyth/js-files/homepage-components/FooterComponent.js
@@ -0,0 +1,26 @@
+const email_id = "someone@gmail.com";
+const contact_no = "+71616711";
+
+
+function FooterComponent(){
+    return (   
+        <div id="footer-content">
+            <p style={{"text-align":"center"}}>Thank you for visiting our website</p>
+
+            <div id="contact-section">
+                <p id="email" className="">
+                    <a style={{ color: "yellow" }} href={"mailto:"+email_id}>
+                        <i className="bi bi-envelope-fill"></i> Email Us
+                    </a>
+                </p>
+                <p id="linkedIn" className="">
+                    <a href={"tel:"+contact_no} style={{ color: "green" }} target="_blank">
+                        <i className="bi bi-telephone-fill"></i> Call Us
+                    </a>
+                </p>
+            </div>
+        </div>
+    );
+}
+
+ReactDOM.render(<FooterComponent />, document.getElementById("footer"));
--- Beriyth/js-files/homepage-components/HeaderComponent.js
+++ Beriyth/js-files/homepage-components/HeaderComponent.js
@@ -1,20 +1,6 @@
 let header_heading = "Beriyth Info Tech"
 let header_description = "Simple creating opportunities and enabling prosperity"
 
-function navComponent(){
-    return (
-        <div id = "nav">
-            <ul>
-                <li><a class="active" href="#home">Home</a></li>
-                <li><a href="#news">News</a></li>
-                <li><a href="#contact">Contact</a></li>
-                <li><a href="#about">About</a></li>
-            </ul>
-        </div>
-    );
-}
-
-
 function HeaderComponent(){
     return (   
         <div id="header-content">
